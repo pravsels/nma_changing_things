4fef575160f7bac4e36599b12ed3afa1f988f279
Praveen Selvaraj <praveen.beatle@gmail.com>
2024-07-17 08:30:57 +0100

    docker run for mac

--- run_docker_container.sh
+++ run_docker_container.sh
@@ -1,6 +1,14 @@
 
+os=$(uname)
+gpu_flag=""
+
+if [ "$os" != "Darwin" ]; then
+    # if not on Mac, include --gpus all
+    gpu_flag="--gpus all"
+fi
+
 xhost +local:
-docker run --rm -it --gpus all --network=host --privileged \
+docker run --rm -it ${gpu_flag} --network=host --privileged \
     -v "$(pwd)":/app \
     --env="DISPLAY=$DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
